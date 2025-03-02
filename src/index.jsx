8955642055d3d063247c92dd2d3b6a2d2de4e2a8
Daniel Okonma <51037438+okonma01@users.noreply.github.com>
2025-03-01 22:24:03 -0500

    fix deploy issues

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
-import "bootstrap/dist/css/bootstrap.css";
-import React from "react";
-import { createRoot } from 'react-dom/client';
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import React from "../node_modules/react";
+import { createRoot } from '../node_modules/react-dom/client';
 import App from "./App";
 
 createRoot(document.getElementById('root')).render(<App/>)
